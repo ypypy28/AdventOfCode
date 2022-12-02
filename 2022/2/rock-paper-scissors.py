4f2bea5a892046141523341120a1b1ee2be1489f
Nikolai Diachkov <ypypy28@gmail.com>
2022-12-02 11:00:39 +0300

    solve part 2

--- 2022/2/rock-paper-scissors.py
+++ 2022/2/rock-paper-scissors.py
@@ -1,17 +1,38 @@
 from enum import Enum
 
+
 class Result(Enum):
     LOSE = 0
     DRAW = 3
     WIN = 6
 
-MY_TURN = {
+
+MY_TURN_PART1 = {
     'X': 1,
     'Y': 2,
     'Z': 3,
 }
 
-def get_result(opponent: str, me: str) -> Result:
+MY_TURN_PART2 = {
+    'A': 1,
+    'B': 2,
+    'C': 3,
+}
+
+RESULT_PART2 = {
+    'X': Result.LOSE,
+    'Y': Result.DRAW,
+    'Z': Result.WIN,
+}
+
+STRATEGIES = {
+    Result.LOSE: {'A': 'C', 'B': 'A', 'C': 'B'},
+    Result.DRAW: {'A': 'A', 'B': 'B', 'C': 'C'},
+    Result.WIN:  {'A': 'B', 'B': 'C', 'C': 'A'},
+}
+
+
+def get_result_part1(opponent: str, me: str) -> Result:
     match (opponent, me):
         case ('A', 'X') | ('B', 'Y') | ('C', 'Z'):
             return Result.DRAW
@@ -22,18 +43,27 @@ def get_result(opponent: str, me: str) -> Result:
         case _:
             raise NotImplemented
 
-def get_round_score(lst: list[str, str]) -> int:
-    return MY_TURN[lst[1]] + get_result(*lst).value
+
+def get_my_shape_score_part2(opponent: str, result: str) -> str:
+    my_shape = STRATEGIES[RESULT_PART2[result]][opponent]
+    return MY_TURN_PART2[my_shape]
+
+
+def get_round_score(lst: list[str, str]) -> tuple[int, int]:
+    return (MY_TURN_PART1[lst[1]] + get_result_part1(*lst).value,
+            get_my_shape_score_part2(*lst) + RESULT_PART2[lst[1]].value)
 
 
 
 if __name__ == "__main__":
-    my_score = 0
+    my_score1 = my_score2 = 0
     with open('input.txt', 'r') as f:
         for line in f:
-            my_score += get_round_score(line.split())
+            score_part1, score_part2 = get_round_score(line.split())
+            my_score1 += score_part1
+            my_score2 += score_part2
 
     print('ANSWER:',
-          f'Part 1: {my_score}',
+          f'Part 1: {my_score1}',
+          f'Part 2: {my_score2}',
           sep='\n')
-
