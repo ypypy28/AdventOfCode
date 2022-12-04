39f48eec6dc49bae6dc18c05a35ae6e37c39a8a9
Nikolai Diachkov <ypypy28@gmail.com>
2022-12-04 15:00:01 +0300

    solve day 4 part 2

--- 2022/4/camp_cleanup.py
+++ 2022/4/camp_cleanup.py
@@ -1,4 +1,4 @@
-one_fully_contains_another = 0
+one_fully_contains_another = overlap = 0
 with open("input.txt", 'r') as f:
     for line in f:
         elf1, elf2 = line.rstrip().split(',')
@@ -6,14 +6,15 @@ with open("input.txt", 'r') as f:
                  for elf in (elf1, elf2))
         assign1, assign2 = tuple(range(l, r+1) for l, r in edges)
         # print(assign1, assign2)
-        commona_assignments = set(assign1) & set(assign2)
+        common_assignments = set(assign1) & set(assign2)
 
-        if len(commona_assignments) == min(len(assign1), len(assign2)):
+        if common_assignments:
+            overlap += 1
+
+        if len(common_assignments) == min(len(assign1), len(assign2)):
             one_fully_contains_another += 1
-
 
 print("ANSWER:",
       f"PART 1: {one_fully_contains_another}",
-      # f"PART 2: {}",
+      f"PART 2: {overlap}",
       sep='\n')
-
