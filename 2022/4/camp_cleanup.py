c324403947eb824fe20e5fc0c6a0c20fa7c53d47
Nikolai Diachkov <ypypy28@gmail.com>
2022-12-04 15:02:30 +0300

    cleanup a bit

--- 2022/4/camp_cleanup.py
+++ 2022/4/camp_cleanup.py
@@ -1,11 +1,9 @@
 one_fully_contains_another = overlap = 0
 with open("input.txt", 'r') as f:
     for line in f:
-        elf1, elf2 = line.rstrip().split(',')
         edges = ((int(n) for n in elf.split('-'))
-                 for elf in (elf1, elf2))
+                 for elf in line.rstrip().split(','))
         assign1, assign2 = tuple(range(l, r+1) for l, r in edges)
-        # print(assign1, assign2)
         common_assignments = set(assign1) & set(assign2)
 
         if common_assignments:
