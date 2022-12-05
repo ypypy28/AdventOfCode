52aee9f42bbc1f14ce43e3827789afa8f456c26b
Nikolai Diachkov <ypypy28@gmail.com>
2022-12-05 13:23:30 +0300

    refactor into functinons

--- 2022/5/supply_stacks.py
+++ 2022/5/supply_stacks.py
@@ -1,35 +1,36 @@
-import sys
+from typing.io import TextIO
 
 
-def count_stacks(filename: str) -> tuple[int, int]:
-    prev = ''
-    height = -1
-    with open(filename, 'r') as f:
-        while (line:= f.readline()) != '\n':
-            height += 1
-            prev = line
-    return int(prev.rstrip().split()[-1]), height
+def read_header(fd: TextIO) -> list[str]:
+    header = []
+    while (line:=f.readline()) != '\n':
+        header.append(line.rstrip('\n'))
+    return header
+
+
+def count_stacks(header: list[str]) -> tuple[int, int]:
+    return int(header[-1].rstrip().split()[-1]), len(header)-1
+
+
+def get_default_stacks(header: list[str]) -> list[list[str]]:
+    len_stacks, height = count_stacks(header)
+    stacks = [None for _ in range(len_stacks)]
+    for i in range(len(header)-1):
+        for j in range(1, len(header[i]), 4):
+            if header[i][j] != ' ':
+                stack_i = (j-1)//4
+                if stacks[stack_i] is None:
+                    stacks[stack_i] = [None for _ in range(height)]
+                stacks[stack_i][height-1] = header[i][j]
+        height -= 1
+
+    return stacks
 
 
 inputfile = "input.txt"
-len_stacks, max_height = count_stacks(inputfile)
-stacks = [None for _ in range(len_stacks)]
-
-with open("input.txt", 'r') as f:
-    # interprete the default cargo stacks
-    height = max_height
-    while (line:= f.readline()) != '\n':
-        line = line.rstrip('\n')
-        if '[' in line:
-            for i in range(1, len(line), 4):
-                if line[i] != ' ':
-                    stack_i = (i-1)//4
-                    if stacks[stack_i] is None:
-                        stacks[stack_i] = ['' for _ in range(height)]
-                    stacks[stack_i][height-1] = line[i]
-        height -=1
-
-
+with open(inputfile, 'r') as f:
+    header = read_header(f)
+    stacks = get_default_stacks(header)
     stacks2 = [[cargo for cargo in st] for st in stacks]
 
     # execute rearrangements
