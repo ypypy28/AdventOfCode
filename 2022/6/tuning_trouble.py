1c029d312e1adfca68e2c70011fb0eade539cf98
Nikolai Diachkov <ypypy28@gmail.com>
2022-12-06 08:55:22 +0300

    solve part 2

--- 2022/6/tuning_trouble.py
+++ 2022/6/tuning_trouble.py
@@ -2,18 +2,31 @@ from collections import deque
 
 
 MARKER_SIZE = 4
-buf = deque(maxlen=MARKER_SIZE)
-cursor = 0
+MESSAGE_START_MARKER_SIZE = 14
+buf_packet = deque(maxlen=MARKER_SIZE)
+buf_message = deque(maxlen=MESSAGE_START_MARKER_SIZE)
+cursor = cursor_packet = cursor_message = 0
 with open("input.txt", 'r') as f:
-    buf.extend(f.read(MARKER_SIZE))
+    stream = f.read(MARKER_SIZE)
+    buf_packet.extend(stream)
+    buf_message.extend(stream)
     cursor += MARKER_SIZE
 
-    while len(set(buf)) != MARKER_SIZE:
-        buf.append(f.read(1))
+    while len(set(buf_packet)) != MARKER_SIZE:
+        stream = f.read(1)
+        buf_packet.append(stream)
+        buf_message.append(stream)
         cursor += 1
 
+    cursor_packet = cursor
+
+    while len(set(buf_message)) != MESSAGE_START_MARKER_SIZE:
+        buf_message.append(f.read(1))
+        cursor += 1
+    cursor_message = cursor
+
 print("ANSWER:",
-      f"PART 1: {cursor}",
-      f"PART 2:",
+      f"PART 1: {cursor_packet}",
+      f"PART 2: {cursor_message}",
       sep='\n')
 
