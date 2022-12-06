bd47a933230c26143ea7aa1f859561043bf5d5f9
Nikolai Diachkov <ypypy28@gmail.com>
2022-12-06 08:58:32 +0300

    refactor variable names

--- 2022/6/tuning_trouble.py
+++ 2022/6/tuning_trouble.py
@@ -1,26 +1,25 @@
 from collections import deque
 
 
-MARKER_SIZE = 4
-MESSAGE_START_MARKER_SIZE = 14
-buf_packet = deque(maxlen=MARKER_SIZE)
-buf_message = deque(maxlen=MESSAGE_START_MARKER_SIZE)
+PACKET_MARKER_SIZE = 4
+MESSAGE_MARKER_SIZE = 14
+buf_packet = deque(maxlen=PACKET_MARKER_SIZE)
+buf_message = deque(maxlen=MESSAGE_MARKER_SIZE)
 cursor = cursor_packet = cursor_message = 0
 with open("input.txt", 'r') as f:
-    stream = f.read(MARKER_SIZE)
+    stream = f.read(PACKET_MARKER_SIZE)
     buf_packet.extend(stream)
     buf_message.extend(stream)
-    cursor += MARKER_SIZE
+    cursor += PACKET_MARKER_SIZE
 
-    while len(set(buf_packet)) != MARKER_SIZE:
+    while len(set(buf_packet)) != PACKET_MARKER_SIZE:
         stream = f.read(1)
         buf_packet.append(stream)
         buf_message.append(stream)
         cursor += 1
-
     cursor_packet = cursor
 
-    while len(set(buf_message)) != MESSAGE_START_MARKER_SIZE:
+    while len(set(buf_message)) != MESSAGE_MARKER_SIZE:
         buf_message.append(f.read(1))
         cursor += 1
     cursor_message = cursor
