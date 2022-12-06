ad521468ea01430e90ee2238f1e1d9a543c4e093
Nikolai Diachkov <ypypy28@gmail.com>
2022-12-06 09:12:23 +0300

    refactor out custom cursor

--- 2022/6/tuning_trouble.py
+++ 2022/6/tuning_trouble.py
@@ -1,3 +1,4 @@
+from array import array
 from collections import deque
 
 
@@ -5,27 +6,24 @@ PACKET_MARKER_SIZE = 4
 MESSAGE_MARKER_SIZE = 14
 buf_packet = deque(maxlen=PACKET_MARKER_SIZE)
 buf_message = deque(maxlen=MESSAGE_MARKER_SIZE)
-cursor = cursor_packet = cursor_message = 0
+start_packet = start_message = -1
 with open("input.txt", 'r') as f:
     stream = f.read(PACKET_MARKER_SIZE)
     buf_packet.extend(stream)
     buf_message.extend(stream)
-    cursor += PACKET_MARKER_SIZE
 
     while len(set(buf_packet)) != PACKET_MARKER_SIZE:
         stream = f.read(1)
         buf_packet.append(stream)
         buf_message.append(stream)
-        cursor += 1
-    cursor_packet = cursor
+    start_packet = f.tell()
 
     while len(set(buf_message)) != MESSAGE_MARKER_SIZE:
         buf_message.append(f.read(1))
-        cursor += 1
-    cursor_message = cursor
+    start_message = f.tell()
 
 print("ANSWER:",
-      f"PART 1: {cursor_packet}",
-      f"PART 2: {cursor_message}",
+      f"PART 1: {start_packet}",
+      f"PART 2: {start_message}",
       sep='\n')
 
