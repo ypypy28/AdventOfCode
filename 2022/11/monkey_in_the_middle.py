2564e13def91fefa58191bfc56a2e12ed567e9fc
Nikolai Diachkov <ypypy28@gmail.com>
2022-12-12 14:41:33 +0300

    maybe solved part 2 (working really slow)

--- 2022/11/monkey_in_the_middle.py
+++ 2022/11/monkey_in_the_middle.py
@@ -4,25 +4,30 @@ import typing
 from heapq import nlargest
 
 
-filename = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+filename = sys.argv[1] if len(sys.argv) > 1 else "test_input.txt"
 
 
 class Monkey:
     def __init__(
         self, starting_items: list[int],
         operation: typing.Callable,
-        test: typing.Callable
+        test: typing.Callable,
+        can_calm: bool = True
     ) -> None:
         self.inspections = 0
         self.items = starting_items
         self.op = operation
         self.test = test
+        if can_calm:
+            self.calm = lambda val: val // 3
+        else:
+            self.calm = lambda val: val
 
     def round(self, monkeys: list["Monkey"]) -> None:
         for item in self.items:
             self.inspections += 1
             item = self.op(item)
-            item //= 3
+            item = self.calm(item)
             next_monkey_id = self.test(item)
             monkeys[next_monkey_id].receive(item)
 
@@ -76,7 +81,7 @@ def parse_test(example: list[str]) -> typing.Callable:
     return test
 
 
-def create_monkey(monkey_data: list[str]):
+def create_monkey(monkey_data: list[str], can_calm: bool):
     start_items = [int(it)
                    for it in (monkey_data[1]
                               .removeprefix('Starting items: ')
@@ -84,28 +89,48 @@ def create_monkey(monkey_data: list[str]):
     operation = parse_operation(monkey_data[2].removeprefix("Operation: "))
     test = parse_test(monkey_data[3:6])
 
-    return Monkey(start_items, operation, test)
+    return Monkey(start_items, operation, test, can_calm)
 
 
-def solve() -> int:
+def play(rounds: int, can_calm: bool=True) -> int:
     monkeys = []
     with open(filename, 'r') as f:
         monkey_data = read_monkey(f)
         while len(monkey_data) == 6:
-            monkeys.append(create_monkey(monkey_data))
+            monkeys.append(create_monkey(monkey_data, can_calm))
             monkey_data = read_monkey(f)
 
-    for _ in range(20):
+    interesting_rounds = {20, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000}
+
+    for i in range(rounds):
         for monkey in monkeys:
             monkey.round(monkeys)
+        if i+1 in interesting_rounds:
+            show_state(i+1, can_calm, monkeys)
+
+
 
     two_largest = nlargest(2, monkeys, key=lambda m: m.inspections)
     return two_largest[0].inspections * two_largest[1].inspections
 
 
+def show_state(round: int, can_calm: bool, monkeys: list[Monkey]) -> None:
+    print(
+        f"\n== After round {round} == ({can_calm=})",
+        *(f"Monkey {i} inspected items {m.inspections} times."
+          for i, m in enumerate(monkeys)),
+        sep='\n'
+    )
+
+
+def solve() -> tuple[int, int]:
+    return play(rounds=20), play(rounds=10000, can_calm=False)
+
+
 if __name__ == "__main__":
-    part1 = solve()
+    part1, part2 = solve()
 
     print("ANSWER:",
           f"Part 1: {part1}",
+          f"Part 2: {part2}",
           sep='\n')
